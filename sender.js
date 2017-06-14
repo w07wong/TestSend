f5a0a1062aefc8375889ebd9f99e4868f27cded1
William Wong <williamwong@sfhs.com>
2017-06-14 09:17:58 -0700

    Test from Atom.io

--- sender.js
+++ sender.js
@@ -4,11 +4,11 @@ var Tx = require('ethereumjs-tx');
 
 //initialize connection
 var web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('insert ethereum node url here'));
+web3.setProvider(new web3.providers.HttpProvider('https://9t1f4yor7v5t521m4ra9xh24ggd2q4-node.ambisafe.co/'));
 
-var toAccount = 'insert sender account address here';
-var fromAccount = 'insert receiver account address here';
-var privateKey = new Buffer('insert private key for sender here', 'hex');
+var toAccount = '0x251fdCe8D07B6194e0e3C3AaBF8307E53Eea71bE';
+var fromAccount = '0x059345dE4c56C80A5d90AD3B170627e2a7339173';
+var privateKey = new Buffer('', 'hex');
 
 var gasPriceHex = web3.toHex(4000000000);
 var gasLimitHex = web3.toHex(21000);
@@ -30,12 +30,13 @@ var tx = new Tx(rawTx);
 tx.sign(privateKey);
 
 var serializedTx = tx.serialize();
+console.log(serializedTx);
 
-web3.eth.sendRawTransaction(serializedTx.toString('hex'), function (err, hash) {
+/*web3.eth.sendRawTransaction(serializedTx.toString('hex'), function (err, hash) {
     if (err) {
         console.log("" + err)
     }
     else {
-        console.log(hash); 
+        console.log(hash);
     }
-});
+});*/
